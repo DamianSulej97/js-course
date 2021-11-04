057874ed8c60d0c701e27a80daf81bac7639a8ee
Damian <s4marx@gmail.com>
2021-11-04 12:49:50 +0100

    logical operators

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-var myAge = 51;
+var myAge = 25;
 
-if (myAge > 30) {
-    document.write("You are over 30!");
-} else if (myAge > 20) {
-    document.write("You are over 20!");
-} else if (myAge > 10) {
-    document.write("You are over 10!");
-} else if (myAge < 10) {
-    document.write("You are not over 10!");
+if (myAge < 18 || myAge > 30  || myAge === 25) {
+    document.write("You are not invited");
+} else {
+    document.write("You are invided!");
 }
-
