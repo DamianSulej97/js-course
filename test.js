90c7f10cdc2d704e9b7f24b4695d622298097c13
Damian <s4marx@gmail.com>
2021-11-04 20:03:53 +0100

    functions

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-var links = document.getElementsByTagName("a");
-
-for (i = 0; i < links.length; i++){
-
-    links[i].className = "link-" + i;
+function getAverage(a,b,c,d,e,f) {
+    
+    var average = (a + b + c + d + e + f) / 6;
+    console.log(average);
+    return average;
 
 }
+
+var myResult = getAverage(7, 12, 53, 62, 23, 31);
+console.log("the average is " + myResult);
