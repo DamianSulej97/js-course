7cab872ffd75eeba4a205cb7b23cd941b3a96cca
Damian <s4marx@gmail.com>
2021-11-05 23:00:36 +0100

    Date objects

--- test.js
+++ test.js
@@ -1,15 +1,20 @@
-var Car = function (maxSpeed, driver) {
-  this.maxSpeed = maxSpeed;
-  this.driver = driver;
-  this.drive = function (speed, time) {
-    console.log(speed * time);
-  };
-  this.logDriver = function () {
-    console.log("driver name is " + this.driver);
-  };
-};
+var birthday = new Date(1974, 1, 13, 9, 31, 20);
+var birthday2 = new Date(1974, 1, 13, 9, 31, 20);
 
-var myCar = new Car(90, "Damian");
-var myCar2 = new Car(34, "Damian2");
-var myCar3 = new Car(58, "Damian3");
-var myCar4 = new Car(10, "Damian4");
+console.log(birthday.getMonth());
+
+console.log(birthday.getFullYear());
+
+console.log(birthday.getDate());
+
+console.log(birthday.getDay());
+
+console.log(birthday.getHours());
+
+console.log(birthday.getTime());
+
+if (birthday.getTime() == birthday2.getTime()) {
+  console.log("birthdays are equal");
+} else {
+  console.log("birthdays are not equal");
+}
