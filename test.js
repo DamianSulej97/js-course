4920f6f55e985b420154760bad8b1b0cde6aaa70
Damian <s4marx@gmail.com>
2021-11-05 14:03:13 +0100

    strings

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-var a = "apple";
-var b = 5;
+var string1 = "abc";
+var string2 = "Bcd";
 
-// podwójna negacja
-if (!isNaN(a)) {
-  console.log("this is not a number");
-} else {
-  console.log("the meaning of life is " + a + b);
-}
+console.log(string1 < string2);
