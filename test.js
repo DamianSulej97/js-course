10729bda5cde5fda2d8780dc82f5b3d1d297a377
Damian <s4marx@gmail.com>
2021-11-06 14:16:05 +0100

    links customization by js

--- test.js
+++ test.js
@@ -1,20 +1,4 @@
-var birthday = new Date(1974, 1, 13, 9, 31, 20);
-var birthday2 = new Date(1974, 1, 13, 9, 31, 20);
+var linki = document.getElementsByTagName("a");
 
-console.log(birthday.getMonth());
-
-console.log(birthday.getFullYear());
-
-console.log(birthday.getDate());
-
-console.log(birthday.getDay());
-
-console.log(birthday.getHours());
-
-console.log(birthday.getTime());
-
-if (birthday.getTime() == birthday2.getTime()) {
-  console.log("birthdays are equal");
-} else {
-  console.log("birthdays are not equal");
-}
+linki[0].innerHTML = "Siema";
+linki[1].innerHTML = "Co tam";
