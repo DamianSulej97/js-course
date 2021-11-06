54855816535926e8086a418ae81896c093e0b1a2
Damian <s4marx@gmail.com>
2021-11-06 23:24:39 +0100

    onClick, attempt to add overflow on js

--- test.js
+++ test.js
@@ -1,9 +1,25 @@
-var linki = document.getElementById("link");
+function setUpEvents() {
+  var content = document.getElementById("content");
+  var button = document.getElementById("show-more");
 
-linki.onclick = function () {
-  alert("you clicked me");
+  button.onclick = function () {
+    if (content.className == "open") {
+      //zmniejszenie boxa
+      content.className = "";
+      button.innerHTML = "Show More";
+      content.setAttribute("style", "overflow: hidden;");
+    } else {
+      //rozszerzenie boxa
+      content.className = "open";
+      button.innerHTML = "Show Less";
+      content.setAttribute("style", "overflow: scroll;");
+    }
+  };
+}
+
+//uruchomienie funkcji po wczytaniu calej strony
+window.onload = function () {
+  setUpEvents();
 };
 
-linki.onmouseover = function () {
-  alert("you hovered over me");
-};
+content.setAttribute("style", "overflow: scroll;");
