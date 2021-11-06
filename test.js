4f4746964d424487759a9be924e14ea63b4aa6db
Damian <s4marx@gmail.com>
2021-11-06 23:50:11 +0100

    js timers

--- test.js
+++ test.js
@@ -1,25 +1,19 @@
-function setUpEvents() {
-  var content = document.getElementById("content");
-  var button = document.getElementById("show-more");
+var colourChanger = document.getElementById("colour-changer");
+var colours = ["red", "blue", "green", "pink"];
+var counter = 0;
 
-  button.onclick = function () {
-    if (content.className == "open") {
-      //zmniejszenie boxa
-      content.className = "";
-      button.innerHTML = "Show More";
-      content.setAttribute("style", "overflow: hidden;");
-    } else {
-      //rozszerzenie boxa
-      content.className = "open";
-      button.innerHTML = "Show Less";
-      content.setAttribute("style", "overflow: scroll;");
-    }
-  };
+function changeColour() {
+  if (counter >= colours.length) {
+    counter = 0;
+  }
+
+  colourChanger.style.background = colours[counter];
+  counter++;
 }
 
-//uruchomienie funkcji po wczytaniu calej strony
-window.onload = function () {
-  setUpEvents();
-};
+var myTimer = setInterval(changeColour, 3000);
 
-content.setAttribute("style", "overflow: scroll;");
+colourChanger.onclick = function () {
+  clearInterval(myTimer);
+  colourChanger.innerHTML = "Timer stopped";
+};
