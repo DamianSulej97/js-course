644848cb8b41ce8262b62e29ba5ce1adabeb3821
Damian <s4marx@gmail.com>
2021-11-13 14:39:29 +0100

    variables and constants

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,13 @@
-alert("hello, world");
+let age = 24;
+let year = 2021;
+
+console.log(age, year);
+
+age = 30;
+console.log(age, year);
+
+const points = 100;
+console.log(points);
+
+points = 50;
+console.log(age, year);
