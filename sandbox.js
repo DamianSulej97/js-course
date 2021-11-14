2136856c48ef2e2a552f5d6d72955b25db6b5009
Damian <s4marx@gmail.com>
2021-11-14 19:03:33 +0100

    strick and loose comparisons

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,14 @@
-// booleans & comparisons
-console.log(true, false, "true", "false");
-
-// methods can return booleans
-let email = "luigi@thenetninja.co.uk";
-let names = ["mario", "luigi", "toad"];
-
-// let result = email.includes("!");
-// let result = names.includes("luigi");
-// console.log(result);
-
-// comparison operators
 let age = 25;
 
+// loose comparison
+
 // console.log(age == 25);
-// console.log(age == 32);
-// console.log(age != 24);
-// console.log(age > 22);
-// console.log(age < 20);
-// console.log(age <= 25);
-// console.log(age >= 25);
+// console.log(age == "25");
 
-let name = "shaun";
+// console.log(age != 25);
+// console.log(age != "25");
 
-console.log(name == "shaun");
-console.log(name == "Shaun");
-console.log(name > "crystal");
-console.log(name > "Shaun");
-console.log(name > "Crystal");
+// strict comparison
+
+console.log(age === 25);
+console.log(age === "25");
